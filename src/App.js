f4981a18e152a55d8fd91a0a529f6449ef06d2ea
zztttt <1315691850@qq.com>
2020-11-29 16:26:34 +0800

    update dynamic chart

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import React from "react";
 
-
-//import EchartsTest from './components/echartTest'
-import BarChart from './components/barChart'
-//import dynamicChart from './components/dynamicChart'
 import Dynamic from "./components/Dynamic";
 
 function App() {
   return (
     <div className="App">
-      <BarChart/>
       <Dynamic/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
